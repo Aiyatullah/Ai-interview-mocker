1725a11b258d790fdbbdb75782031cf6b3d36903
Aiyatullah <114688719+Aiyatullah@users.noreply.github.com>
2025-05-14 12:48:50 +0530

    Update firebase.js

--- utils/firebase.js
+++ utils/firebase.js
@@ -1,22 +1,21 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore"; // ✅ Import Firestore
 import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDu5gLyCa0NAuNHcQ-z10PvZb5G7AmVoYs",
-  authDomain: "ai-interview-mocker-a9d05.firebaseapp.com",
-  projectId: "ai-interview-mocker-a9d05",
-  storageBucket: "ai-interview-mocker-a9d05.firebasestorage.app",
-  messagingSenderId: "946667289499",
-  appId: "1:946667289499:web:32291c6801ec8ad9327f3e",
-  measurementId: "G-XHGGM1DD3F"
+  apiKey: "AIzaSyBfgayyA3uX6r1I-THobosCN-Xe9IxSi2U",
+  authDomain: "ai-interview-20a89.firebaseapp.com",
+  projectId: "ai-interview-20a89",
+  storageBucket: "ai-interview-20a89.firebasestorage.app",
+  messagingSenderId: "769519639916",
+  appId: "1:769519639916:web:354ab97f430d537fe72749",
+  measurementId: "G-288L6EP173"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const db = getFirestore(app); // ✅ Initialize Firestore
-
-
-export { db };
+const analytics = getAnalytics(app);
